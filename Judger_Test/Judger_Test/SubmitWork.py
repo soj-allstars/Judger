3a2166dc4adec1c71fcf14e4e8a039cb3a1d49e9
LUEVE <724818484@qq.com>
2019-12-02 18:28:20 +0800

    a test for rq

---
+++ Judger_Test/Judger_Test/SubmitWork.py
@@ -0,0 +1,16 @@
+from rq import Queue
+import json
+from redis import Redis
+import time
+
+q = Queue(connection = Redis("47.106.140.231", "6379"))
+test_dict = dict()
+fo = open("testjson.json", "r")
+test_dict = json.load(fo)                             
+result = q.enqueue("QueueDealer.deal_submitted", **test_dict)
+
+print("submit-----")
+time.sleep(1)
+print(result.result)
+
+pass
