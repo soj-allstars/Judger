024cca0e0bd8a6fefc104a1061a8e6af2be78be6
LUEVE <724818484@qq.com>
2019-12-02 18:48:53 +0800

    fix name

--- Judger_Test/Judger_Test/submit_work.py
+++ Judger_Test/Judger_Test/submit_work.py
@@ -7,7 +7,7 @@ q = Queue(connection = Redis("47.106.140.231", "6379"))
 test_dict = dict()
 fo = open("testjson.json", "r")
 test_dict = json.load(fo)                             
-result = q.enqueue("QueueDealer.deal_submitted", **test_dict)
+result = q.enqueue("queue_dealer.deal_submitted", **test_dict)
 
 print("submit-----")
 time.sleep(1)
