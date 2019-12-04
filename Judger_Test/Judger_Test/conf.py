8911b2819ff46981e15721d884ea84f9eee940f3
Swing <sgnxydx3211@163.com>
2019-12-04 23:29:45 +0800

    fix: send_result_back is now done in judge job; rename 'deal_submitted' to 'judge_submission'

--- Judger_Test/Judger_Test/conf.py
+++ Judger_Test/Judger_Test/conf.py
@@ -2,7 +2,8 @@ import os
 from redis import Redis
 from rq import Queue
 
-SOJ_HOST = os.environ.get('SOJ_HOST', '127.0.0.1')
+SOJ_HOST = os.environ.get('SOJ_HOST', '47.106.140.231')
+RESULT_API_URL = f'{SOJ_HOST}/api/judge/send-result/'
 
 redis = Redis(os.environ.get('REDIS_HOST', '47.106.140.231'))
 judge_q = Queue('judge', connection=redis)
