babf8bbba4fa52a165a68eeb4bca79e2f6b16db9
Swing <sgnxydx3211@163.com>
2020-02-15 22:02:51 +0800

    chore: Dockerfile and setup.py

--- Judger_Test/Judger_Test/setup.sh
+++ Judger_Test/Judger_Test/setup.sh
@@ -4,13 +4,18 @@ export LOG_DIR=logs
 mkdir -p $LOG_DIR
 mkdir -p problems
 
-pip install -i https://pypi.tuna.tsinghua.edu.cn/simple --upgrade pip
+groupadd fuse
+usermod -a -G fuse root
+
+pip install -i https://pypi.tuna.tsinghua.edu.cn/simple --no-cache-dir --upgrade pip
 pip install -i https://pypi.tuna.tsinghua.edu.cn/simple --no-cache-dir -r requirements.txt
 
 git clone https://github.com/jpswing/Lo-runner.git
 cd Lo-runner
 python setup.py install
 cd ..
+
+rm -rf Lo-runner
 
 git clone https://github.com/MikeMirzayanov/testlib.git
 cd testlib
@@ -25,6 +30,6 @@ do
 done
 cd ..
 
-apt install pypy3
-apt install default-jdk
-apt install sshfs
+apt-get update
+apt-get install -y pypy3 default-jdk sshfs
+rm -rf /var/lib/apt/lists/*
--- readme.md
+++ readme.md
@@ -1,5 +1,4 @@
-python package
-https://github.com/dojiong/Lo-runner/
+https://github.com/jpswing/Lo-runner.git
 
 在Judger_Test/Judger_Test文件目录下打开cmd
 
@@ -8,3 +7,11 @@ docker image build -t judger:1.0 .
 之后
 
 docker run -it judger:1.0
+
+### SSHFS
+[tutorial](https://www.linode.com/docs/networking/ssh/using-sshfs-on-linux/)
+
+dev:
+```
+sshfs root@172.17.0.1:/root/soj/shared_data/problems problems
+```
