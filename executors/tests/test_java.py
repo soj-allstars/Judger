133191cf3ca55d6aff049c04fd2983ea8d45d7ef
LUEVE <724818484@qq.com>
2020-04-03 18:52:27 +0800

    name java class
    you can name for java class

--- executors/tests/test_java.py
+++ executors/tests/test_java.py
@@ -57,7 +57,7 @@ public class Solution {
 
     def test_CE(self):
         ce_code = """
-                    import java.util.Scanner;
+        import java.util.Scanner;
         public class Solution {
             public static void main(String[] args) {
                 Scanner sc=new Scanner(System.in);
@@ -174,6 +174,29 @@ public class Solution {
         }, self.submission_dir, submitted_executor)
         print(result['desc'])
         self.assertEqual(result['verdict'], VerdictResult.OLE)
+
+    def testClassName(self):
+        ac_code = """
+        import java.util.Scanner;
+        public class Fuck {
+            public static void main(String[] args) {
+                Scanner sc=new Scanner(System.in);
+                int a=sc.nextInt(),b=sc.nextInt();
+                System.out.println(a+b);
+            }
+        }
+        """
+        submitted_executor = get_executor(self.submission_lang, ac_code, f'{self.submission_dir}/submitted')
+        result = do_judge({
+            "submit_id": 1,
+            "problem_id": 1,
+            "time_limit": 5000,
+            "memory_limit": 262144,
+            "checker_type": "icmp",
+        }, self.submission_dir, submitted_executor)
+        print(result['desc'])
+        self.assertEqual(result['verdict'], VerdictResult.AC)
+        pass
 
     def tearDown(self) -> None:
         shutil.rmtree(self.submission_dir, ignore_errors=True)
